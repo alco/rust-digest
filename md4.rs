7557a9fba0ed51133a7961d6957783d90f7e3fc9
Alexei Sholik <alcosholik@gmail.com>
2013-07-28 18:27:29 +0300

    Clean up tests for md4

--- md4.rs
+++ md4.rs
@@ -34,13 +34,11 @@ fn add_input(st: &mut Md4, msg: &[u8]) {
         st.msg_block_idx += 1;
         st.msg_len += 8;
         if st.msg_len == 0 {
-            // Message length overflow
-
             // FIXME: Need better failure mode (#2346)
             fail!();
         }
-        if st.msg_block_idx == MSG_BLOCK_LEN { 
-            process_msg_block(st); 
+        if st.msg_block_idx == MSG_BLOCK_LEN {
+            process_msg_block(st);
             st.msg_block_idx = 0;
         }
     }
@@ -52,15 +50,15 @@ fn process_msg_block(st: &mut Md4) {
         let r = r as u32;
         (x << r) | (x >> (32u32 - r))
     }
-    
-    let mut x = &st.work_buf;
+
+    let x = &mut st.work_buf;
 
     // Copy msg_block to x
     let m = &st.msg_block;
     let mut x_i = 0u;
     let mut m_i = 0u;
     while x_i < 16u {
-        x[x_i] = (m[m_i] as u32) 
+        x[x_i] = (m[m_i] as u32)
                | (m[m_i+1u] as u32 << 8u32)
                | (m[m_i+2u] as u32 << 16u32)
                | (m[m_i+3u] as u32 << 24u32);
@@ -113,8 +111,8 @@ fn process_msg_block(st: &mut Md4) {
     }
 
     // Update the buffer
-    st.h[0] += a; 
-    st.h[1] += b; 
+    st.h[0] += a;
+    st.h[1] += b;
     st.h[2] += c;
     st.h[3] += d;
 }
@@ -139,11 +137,11 @@ fn pad_msg_block(st: &mut Md4, len: uint) {
     let mut i = st.msg_block_idx+1;
     while i < len {
         st.msg_block[i] = 0u8;
+        i += 1;
     }
 }
 
 fn pad_msg(st: &mut Md4) {
-    // Pad message
     if st.msg_block_idx >= MSG_BLOCK_LEN-8 {
         // Process last block before appending length
         pad_msg_block(st, MSG_BLOCK_LEN);
@@ -184,8 +182,8 @@ impl Md4 {
 impl Digest for Md4 {
     pub fn reset(&mut self) {
         self.h = [0x67452301u32,
-                  0xefcdab89u32, 
-                  0x98badcfeu32, 
+                  0xefcdab89u32,
+                  0x98badcfeu32,
                   0x10325476u32];
         self.msg_len = 0;
         self.msg_block_idx = 0;
@@ -196,10 +194,10 @@ impl Digest for Md4 {
     pub fn output_bits(&self) -> uint { 128 } // static?
 }
 
-#[test]
-fn test_md4() {
+#[cfg(test)]
+mod tests {
     use extra::digest::{Digest, DigestUtil};
-    use extra::sha1::Sha1;
+    use Md4;
 
     #[deriving(Clone)]
     struct Test {
@@ -210,138 +208,44 @@ fn test_md4() {
 
     #[test]
     fn test() {
-        fn a_million_letter_a() -> ~str {
-            let mut i = 0;
-            let mut rs = ~"";
-            while i < 100000 {
-                rs.push_str("aaaaaaaaaa");
-                i += 1;
-            }
-            return rs;
-        }
-        // Test messages from FIPS 180-1
-        let fips_180_1_tests = ~[
+        let wikipedia_tests = ~[
             Test {
                 input: ~"",
                 output: ~[
-                    0xA9u8, 0x99u8, 0x3Eu8, 0x36u8,
-                    0x47u8, 0x06u8, 0x81u8, 0x6Au8,
-                    0xBAu8, 0x3Eu8, 0x25u8, 0x71u8,
-                    0x78u8, 0x50u8, 0xC2u8, 0x6Cu8,
+                    0x31u8, 0xd6u8, 0xcfu8, 0xe0u8,
+                    0xd1u8, 0x6au8, 0xe9u8, 0x31u8,
+                    0xb7u8, 0x3cu8, 0x59u8, 0xd7u8,
+                    0xe0u8, 0xc0u8, 0x89u8, 0xc0u8,
                 ],
                 output_str: ~"31d6cfe0d16ae931b73c59d7e0c089c0",
             },
             Test {
-                input: ~"a",
-                output: ~[
-                    0xA9u8, 0x99u8, 0x3Eu8, 0x36u8,
-                    0x47u8, 0x06u8, 0x81u8, 0x6Au8,
-                    0xBAu8, 0x3Eu8, 0x25u8, 0x71u8,
-                    0x78u8, 0x50u8, 0xC2u8, 0x6Cu8,
-                ],
-                output_str: ~"bde52cb31de33e46245e05fbdbd6fb24",
-            },
-            Test {
-                input: ~"abc",
-                output: ~[
-                    0xA9u8, 0x99u8, 0x3Eu8, 0x36u8,
-                    0x47u8, 0x06u8, 0x81u8, 0x6Au8,
-                    0xBAu8, 0x3Eu8, 0x25u8, 0x71u8,
-                    0x78u8, 0x50u8, 0xC2u8, 0x6Cu8,
-                ],
-                output_str: ~"a448017aaf21d8525fc10ae87aa6729d",
-            },
-            Test {
-                input:
-                     ~"message digest",
-                output: ~[
-                    0x84u8, 0x98u8, 0x3Eu8, 0x44u8,
-                    0x1Cu8, 0x3Bu8, 0xD2u8, 0x6Eu8,
-                    0xBAu8, 0xAEu8, 0x4Au8, 0xA1u8,
-                    0xF9u8, 0x51u8, 0x29u8, 0xE5u8,
-                ],
-                output_str: ~"d9130a8164549fe818874806e1c7014b",
-            },
-            Test {
-                input:
-                     ~"abcdefghijklmnopqrstuvwxyz",
-                output: ~[
-                    0x84u8, 0x98u8, 0x3Eu8, 0x44u8,
-                    0x1Cu8, 0x3Bu8, 0xD2u8, 0x6Eu8,
-                    0xBAu8, 0xAEu8, 0x4Au8, 0xA1u8,
-                    0xF9u8, 0x51u8, 0x29u8, 0xE5u8,
-                ],
-                output_str: ~"d79e1c308aa5bbcdeea8ed63df412da9",
-            },
-            Test {
-                input:
-                     ~"ABCDEFGHIJKLMNOPQRSTUVWXYZ" + 
-                      "abcdefghijklmnopqrstuvwxyz" +
-                      "0123456789",
-                output: ~[
-                    0x84u8, 0x98u8, 0x3Eu8, 0x44u8,
-                    0x1Cu8, 0x3Bu8, 0xD2u8, 0x6Eu8,
-                    0xBAu8, 0xAEu8, 0x4Au8, 0xA1u8,
-                    0xF9u8, 0x51u8, 0x29u8, 0xE5u8,
-                ],
-                output_str: ~"043f8582f241db351ce627e153e7f0e4",
-            },
-            Test {
-                input:
-                     ~"123456789012345678901234567890" +
-                      "123456789012345678901234567890" +
-                      "12345678901234567890",
-                output: ~[
-                    0x84u8, 0x98u8, 0x3Eu8, 0x44u8,
-                    0x1Cu8, 0x3Bu8, 0xD2u8, 0x6Eu8,
-                    0xBAu8, 0xAEu8, 0x4Au8, 0xA1u8,
-                    0xF9u8, 0x51u8, 0x29u8, 0xE5u8,
-                ],
-                output_str: ~"e33b4ddc9c38f2199c3e7b164fcc0536",
-            },
-            Test {
-                input: a_million_letter_a(),
-                output: ~[
-                    0x34u8, 0xAAu8, 0x97u8, 0x3Cu8,
-                    0xD4u8, 0xC4u8, 0xDAu8, 0xA4u8,
-                    0xF6u8, 0x1Eu8, 0xEBu8, 0x2Bu8,
-                    0xDBu8, 0xADu8, 0x27u8, 0x31u8,
-                ],
-                output_str: ~"34aa973cd4c4daa4f61eeb2bdbad2731",
-            },
-        ];
-        // Examples from wikipedia
-
-        let wikipedia_tests = ~[
-            Test {
                 input: ~"The quick brown fox jumps over the lazy dog",
                 output: ~[
-                    0x2fu8, 0xd4u8, 0xe1u8, 0xc6u8,
-                    0x7au8, 0x2du8, 0x28u8, 0xfcu8,
-                    0xedu8, 0x84u8, 0x9eu8, 0xe1u8,
-                    0xbbu8, 0x76u8, 0xe7u8, 0x39u8,
-                    0x1bu8, 0x93u8, 0xebu8, 0x12u8,
+                    0x1bu8, 0xeeu8, 0x69u8, 0xa4u8,
+                    0x6bu8, 0xa8u8, 0x11u8, 0x18u8,
+                    0x5cu8, 0x19u8, 0x47u8, 0x62u8,
+                    0xabu8, 0xaeu8, 0xaeu8, 0x90u8,
                 ],
-                output_str: ~"2fd4e1c67a2d28fced849ee1bb76e7391b93eb12",
+                output_str: ~"1bee69a46ba811185c194762abaeae90",
             },
             Test {
                 input: ~"The quick brown fox jumps over the lazy cog",
                 output: ~[
-                    0xdeu8, 0x9fu8, 0x2cu8, 0x7fu8,
-                    0xd2u8, 0x5eu8, 0x1bu8, 0x3au8,
-                    0xfau8, 0xd3u8, 0xe8u8, 0x5au8,
-                    0x0bu8, 0xd1u8, 0x7du8, 0x9bu8,
-                    0x10u8, 0x0du8, 0xb4u8, 0xb3u8,
+                    0xb8u8, 0x6eu8, 0x13u8, 0x0cu8,
+                    0xe7u8, 0x02u8, 0x8du8, 0xa5u8,
+                    0x9eu8, 0x67u8, 0x2du8, 0x56u8,
+                    0xadu8, 0x01u8, 0x13u8, 0xdfu8,
                 ],
-                output_str: ~"de9f2c7fd25e1b3afad3e85a0bd17d9b100db4b3",
+                output_str: ~"b86e130ce7028da59e672d56ad0113df",
             },
         ];
-        let tests = fips_180_1_tests + wikipedia_tests;
+        let tests = wikipedia_tests;
 
-        // Test that it works when accepting the message all at once
 
         let mut out = [0u8, ..16];
 
+        // Test that it works when accepting the message all at once
         let mut md = ~Md4::new();
         for tests.iter().advance |t| {
             (*md).input_str(t.input);
@@ -354,7 +258,6 @@ fn test_md4() {
 
             md.reset();
         }
-
 
         // Test that it works when accepting the message in pieces
         for tests.iter().advance |t| {
@@ -369,7 +272,7 @@ fn test_md4() {
             assert!(t.output.as_slice() == out);
 
             let out_str = (*md).result_str();
-            assert_eq!(out_str.len(), 40);
+            assert_eq!(out_str.len(), 32);
             assert!(out_str == t.output_str);
 
             md.reset();

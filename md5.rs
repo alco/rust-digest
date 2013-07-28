f0715603907cc13396b26269316f357b32cd1a43
Alexei Sholik <alcosholik@gmail.com>
2013-07-28 19:08:26 +0300

    CLean up md5 and add tests

--- md5.rs
+++ md5.rs
@@ -9,13 +9,14 @@
 // except according to those terms.
 
 
-use digest::Digest;
+extern mod extra;
+use extra::digest::Digest;
 
 // Some unexported constants
 static DIGEST_BUF_LEN: uint = 4u; // 4 32-bit words
 static WORK_BUF_LEN: uint = 16u;  // 16 32-bit words
 static MSG_BLOCK_LEN: uint = 64u; // 512 bit
-static K_TAB: [u32, ..64] = 
+static K_TAB: [u32, ..64] =
    [0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
     0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
     0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
@@ -53,13 +54,11 @@ fn add_input(st: &mut Md5, msg: &[u8]) {
         st.msg_block_idx += 1;
         st.msg_len += 8;
         if st.msg_len == 0 {
-            // Message length overflow
-
             // FIXME: Need better failure mode (#2346)
             fail!();
         }
-        if st.msg_block_idx == MSG_BLOCK_LEN { 
-            process_msg_block(st); 
+        if st.msg_block_idx == MSG_BLOCK_LEN {
+            process_msg_block(st);
             st.msg_block_idx = 0;
         }
     }
@@ -71,15 +70,15 @@ fn process_msg_block(st: &mut Md5) {
         let r = r as u32;
         (x << r) | (x >> (32u32 - r))
     }
-    
-    let mut x = &st.work_buf;
+
+    let x = &mut st.work_buf;
 
     // Copy msg_block to x
     let m = &st.msg_block;
     let mut x_i = 0u;
     let mut m_i = 0u;
     while x_i < 16u {
-        x[x_i] = (m[m_i] as u32) 
+        x[x_i] = (m[m_i] as u32)
                | (m[m_i+1u] as u32 << 8u32)
                | (m[m_i+2u] as u32 << 16u32)
                | (m[m_i+3u] as u32 << 24u32);
@@ -87,7 +86,10 @@ fn process_msg_block(st: &mut Md5) {
         m_i += 4u;
     }
 
-    let mut [a, b, c, d] = st.h;
+    let mut a = st.h[0];
+    let mut b = st.h[1];
+    let mut c = st.h[2];
+    let mut d = st.h[3];
 
     // Round 1
     // F(X,Y,Z) = XY v not(X) Z
@@ -109,51 +111,49 @@ fn process_msg_block(st: &mut Md5) {
     let mut k_i = 16u;
     while k_i < 32u {
         a = b + rot( 5, a + ((b & d) | (c & !d)) + x[x_i] + K_TAB[k_i]);
-        x_i = (x_i + 5u) & 0xFFu;
+        x_i = (x_i + 5u) & 0xFu;
         d = a + rot( 9, d + ((a & c) | (b & !c)) + x[x_i] + K_TAB[k_i+1]);
-        x_i = (x_i + 5u) & 0xFFu;
+        x_i = (x_i + 5u) & 0xFu;
         c = d + rot(14, c + ((d & b) | (a & !b)) + x[x_i] + K_TAB[k_i+2]);
-        x_i = (x_i + 5u) & 0xFFu;
+        x_i = (x_i + 5u) & 0xFu;
         b = c + rot(20, b + ((c & a) | (d & !a)) + x[x_i] + K_TAB[k_i+3]);
-        x_i = (x_i + 5u) & 0xFFu;
+        x_i = (x_i + 5u) & 0xFu;
         k_i += 4u;
     }
 
     // Round 3
     // H(X,Y,Z) = X xor Y xor Z
     let mut x_i = 5u;
-    let mut k_i = 32u;
     while k_i < 48u {
         a = b + rot( 4, a + (b ^ c ^ d) + x[x_i] + K_TAB[k_i]);
-        x_i = (x_i + 3) & 0xFFu;
+        x_i = (x_i + 3) & 0xFu;
         d = a + rot(11, d + (a ^ b ^ c) + x[x_i] + K_TAB[k_i+1]);
-        x_i = (x_i + 3) & 0xFFu;
+        x_i = (x_i + 3) & 0xFu;
         c = d + rot(16, c + (d ^ a ^ b) + x[x_i] + K_TAB[k_i+2]);
-        x_i = (x_i + 3) & 0xFFu;
+        x_i = (x_i + 3) & 0xFu;
         b = c + rot(23, b + (c ^ d ^ a) + x[x_i] + K_TAB[k_i+3]);
-        x_i = (x_i + 3) & 0xFFu;
+        x_i = (x_i + 3) & 0xFu;
         k_i += 4u;
     }
 
     // Round 4
     // I(X,Y,Z) = Y xor (X v not(Z))
     let mut x_i = 0u;
-    let mut k_i = 48u;
     while k_i < 64u {
         a = b + rot( 6, a + (c ^ (b | !d)) + x[x_i] + K_TAB[k_i]);
-        x_i = (x_i + 7) & 0xFFu;
+        x_i = (x_i + 7) & 0xFu;
         d = a + rot(10, d + (b ^ (a | !c)) + x[x_i] + K_TAB[k_i+1]);
-        x_i = (x_i + 7) & 0xFFu;
+        x_i = (x_i + 7) & 0xFu;
         c = d + rot(15, c + (a ^ (d | !b)) + x[x_i] + K_TAB[k_i+2]);
-        x_i = (x_i + 7) & 0xFFu;
+        x_i = (x_i + 7) & 0xFu;
         b = c + rot(21, b + (d ^ (c | !a)) + x[x_i] + K_TAB[k_i+3]);
-        x_i = (x_i + 7) & 0xFFu;
+        x_i = (x_i + 7) & 0xFu;
         k_i += 4u;
     }
 
     // Update the buffer
-    st.h[0] += a; 
-    st.h[1] += b; 
+    st.h[0] += a;
+    st.h[1] += b;
     st.h[2] += c;
     st.h[3] += d;
 }
@@ -163,7 +163,7 @@ fn mk_result(st: &mut Md5, rs: &mut [u8]) {
     let mut i = 0u;
     let mut r_i = 0u;
     while i < 4u {
-        let w = h[i];
+        let w = st.h[i];
         rs[r_i] = (w & 0xFFu32) as u8;
         rs[r_i+1] = ((w >> 8u32) & 0xFFu32) as u8;
         rs[r_i+2] = ((w >> 16u32) & 0xFFu32) as u8;
@@ -173,30 +173,35 @@ fn mk_result(st: &mut Md5, rs: &mut [u8]) {
     }
 }
 
-fn pad_msg_block(st: &mut Md5, len: uint) {
-    st.msg_block[st.msg_block_idx] = 0x80u8;
-    let mut i = st.msg_block_idx+1;
+fn append_zeros(st: &mut Md5, mut i: uint, len: uint) {
     while i < len {
         st.msg_block[i] = 0u8;
+        i += 1;
     }
 }
 
+fn pad_msg_block(st: &mut Md5, len: uint) {
+    st.msg_block[st.msg_block_idx] = 0x80u8;   // 1 bit
+    append_zeros(st, st.msg_block_idx+1, len);
+}
+
 fn pad_msg(st: &mut Md5) {
-    // Pad message
-    if st.msg_block_idx >= MSG_BLOCK_LEN-8 {
+    static MSG_BLOCK_PAD_LEN: uint = MSG_BLOCK_LEN - 8;
+
+    if st.msg_block_idx >= MSG_BLOCK_PAD_LEN {
         // Process last block before appending length
         pad_msg_block(st, MSG_BLOCK_LEN);
         process_msg_block(st);
-    }
-    if st.msg_block_idx < MSG_BLOCK_LEN-8 {
-        pad_msg_block(st, MSG_BLOCK_LEN-8);
+        append_zeros(st, 0, MSG_BLOCK_PAD_LEN);
+    } else {
+        pad_msg_block(st, MSG_BLOCK_PAD_LEN);
     }
 
     // Append length
     let mut i = 0u;
     let mut len = st.msg_len;
     while i < 8u {
-        st.msg_block[MSG_BLOCK_LEN-8+i] = (len & 0xFFu64) as u8;
+        st.msg_block[MSG_BLOCK_PAD_LEN+i] = (len & 0xFFu64) as u8;
         len >>= 8u64;
         i += 1;
     }
@@ -212,7 +217,7 @@ impl Md5 {
             msg_len: 0u64,
             msg_block: [0u8, ..MSG_BLOCK_LEN],
             msg_block_idx: 0,
-            work_buf: [0u32, ..WORK_BUF_LEN]
+            work_buf: [0u32, ..WORK_BUF_LEN],
             computed: false,
         };
         st.reset();
@@ -223,8 +228,8 @@ impl Md5 {
 impl Digest for Md5 {
     pub fn reset(&mut self) {
         self.h = [0x67452301u32,
-                  0xefcdab89u32, 
-                  0x98badcfeu32, 
+                  0xefcdab89u32,
+                  0x98badcfeu32,
                   0x10325476u32];
         self.msg_len = 0;
         self.msg_block_idx = 0;
@@ -235,10 +240,10 @@ impl Digest for Md5 {
     pub fn output_bits(&self) -> uint { 128 }
 }
 
-#[test]
-fn test_md5() {
-    use digest::{Digest, DigestUtil};
-    use sha1::Sha1;
+#[cfg(test)]
+mod tests {
+    use extra::digest::{Digest, DigestUtil};
+    use Md5;
 
     #[deriving(Clone)]
     struct Test {
@@ -249,139 +254,110 @@ fn test_md5() {
 
     #[test]
     fn test() {
-        fn a_million_letter_a() -> ~str {
-            let mut i = 0;
-            let mut rs = ~"";
-            while i < 100000 {
-                rs.push_str("aaaaaaaaaa");
-                i += 1;
-            }
-            return rs;
-        }
-        // Test messages from FIPS 180-1
-        let fips_180_1_tests = ~[
+        let wikipedia_tests = ~[
             Test {
                 input: ~"",
                 output: ~[
-                    0xA9u8, 0x99u8, 0x3Eu8, 0x36u8,
-                    0x47u8, 0x06u8, 0x81u8, 0x6Au8,
-                    0xBAu8, 0x3Eu8, 0x25u8, 0x71u8,
-                    0x78u8, 0x50u8, 0xC2u8, 0x6Cu8,
+                    0xd4u8, 0x1du8, 0x8cu8, 0xd9u8,
+                    0x8fu8, 0x00u8, 0xb2u8, 0x04u8,
+                    0xe9u8, 0x80u8, 0x09u8, 0x98u8,
+                    0xecu8, 0xf8u8, 0x42u8, 0x7eu8,
                 ],
-                output_str: ~"31d6cfe0d16ae931b73c59d7e0c089c0",
+                output_str: ~"d41d8cd98f00b204e9800998ecf8427e",
             },
+            Test {
+                input: ~"The quick brown fox jumps over the lazy dog",
+                output: ~[
+                    0x9eu8, 0x10u8, 0x7du8, 0x9du8,
+                    0x37u8, 0x2bu8, 0xb6u8, 0x82u8,
+                    0x6bu8, 0xd8u8, 0x1du8, 0x35u8,
+                    0x42u8, 0xa4u8, 0x19u8, 0xd6u8,
+                ],
+                output_str: ~"9e107d9d372bb6826bd81d3542a419d6",
+            },
+            Test {
+                input: ~"The quick brown fox jumps over the lazy dog.",
+                output: ~[
+                    0xe4u8, 0xd9u8, 0x09u8, 0xc2u8,
+                    0x90u8, 0xd0u8, 0xfbu8, 0x1cu8,
+                    0xa0u8, 0x68u8, 0xffu8, 0xadu8,
+                    0xdfu8, 0x22u8, 0xcbu8, 0xd0u8,
+                ],
+                output_str: ~"e4d909c290d0fb1ca068ffaddf22cbd0",
+            },
+        ];
+        let test_vectors = ~[
             Test {
                 input: ~"a",
                 output: ~[
-                    0xA9u8, 0x99u8, 0x3Eu8, 0x36u8,
-                    0x47u8, 0x06u8, 0x81u8, 0x6Au8,
-                    0xBAu8, 0x3Eu8, 0x25u8, 0x71u8,
-                    0x78u8, 0x50u8, 0xC2u8, 0x6Cu8,
+                    0x0cu8, 0xc1u8, 0x75u8, 0xb9u8,
+                    0xc0u8, 0xf1u8, 0xb6u8, 0xa8u8,
+                    0x31u8, 0xc3u8, 0x99u8, 0xe2u8,
+                    0x69u8, 0x77u8, 0x26u8, 0x61u8,
                 ],
-                output_str: ~"bde52cb31de33e46245e05fbdbd6fb24",
+                output_str: ~"0cc175b9c0f1b6a831c399e269772661",
             },
             Test {
                 input: ~"abc",
                 output: ~[
-                    0xA9u8, 0x99u8, 0x3Eu8, 0x36u8,
-                    0x47u8, 0x06u8, 0x81u8, 0x6Au8,
-                    0xBAu8, 0x3Eu8, 0x25u8, 0x71u8,
-                    0x78u8, 0x50u8, 0xC2u8, 0x6Cu8,
+                    0x90u8, 0x01u8, 0x50u8, 0x98u8,
+                    0x3cu8, 0xd2u8, 0x4fu8, 0xb0u8,
+                    0xd6u8, 0x96u8, 0x3fu8, 0x7du8,
+                    0x28u8, 0xe1u8, 0x7fu8, 0x72u8,
                 ],
-                output_str: ~"a448017aaf21d8525fc10ae87aa6729d",
+                output_str: ~"900150983cd24fb0d6963f7d28e17f72",
             },
             Test {
-                input:
-                     ~"message digest",
+                input: ~"message digest",
                 output: ~[
-                    0x84u8, 0x98u8, 0x3Eu8, 0x44u8,
-                    0x1Cu8, 0x3Bu8, 0xD2u8, 0x6Eu8,
-                    0xBAu8, 0xAEu8, 0x4Au8, 0xA1u8,
-                    0xF9u8, 0x51u8, 0x29u8, 0xE5u8,
+                    0xf9u8, 0x6bu8, 0x69u8, 0x7du8,
+                    0x7cu8, 0xb7u8, 0x93u8, 0x8du8,
+                    0x52u8, 0x5au8, 0x2fu8, 0x31u8,
+                    0xaau8, 0xf1u8, 0x61u8, 0xd0u8,
                 ],
-                output_str: ~"d9130a8164549fe818874806e1c7014b",
+                output_str: ~"f96b697d7cb7938d525a2f31aaf161d0",
             },
             Test {
-                input:
-                     ~"abcdefghijklmnopqrstuvwxyz",
+                input: ~"abcdefghijklmnopqrstuvwxyz",
                 output: ~[
-                    0x84u8, 0x98u8, 0x3Eu8, 0x44u8,
-                    0x1Cu8, 0x3Bu8, 0xD2u8, 0x6Eu8,
-                    0xBAu8, 0xAEu8, 0x4Au8, 0xA1u8,
-                    0xF9u8, 0x51u8, 0x29u8, 0xE5u8,
+                    0xc3u8, 0xfcu8, 0xd3u8, 0xd7u8,
+                    0x61u8, 0x92u8, 0xe4u8, 0x00u8,
+                    0x7du8, 0xfbu8, 0x49u8, 0x6cu8,
+                    0xcau8, 0x67u8, 0xe1u8, 0x3bu8,
                 ],
-                output_str: ~"d79e1c308aa5bbcdeea8ed63df412da9",
+                output_str: ~"c3fcd3d76192e4007dfb496cca67e13b",
             },
             Test {
-                input:
-                     ~"ABCDEFGHIJKLMNOPQRSTUVWXYZ" + 
-                      "abcdefghijklmnopqrstuvwxyz" +
-                      "0123456789",
+                input: ~"ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                        "abcdefghijklmnopqrstuvwxyz" +
+                        "0123456789",
                 output: ~[
-                    0x84u8, 0x98u8, 0x3Eu8, 0x44u8,
-                    0x1Cu8, 0x3Bu8, 0xD2u8, 0x6Eu8,
-                    0xBAu8, 0xAEu8, 0x4Au8, 0xA1u8,
-                    0xF9u8, 0x51u8, 0x29u8, 0xE5u8,
+                    0xd1u8, 0x74u8, 0xabu8, 0x98u8,
+                    0xd2u8, 0x77u8, 0xd9u8, 0xf5u8,
+                    0xa5u8, 0x61u8, 0x1cu8, 0x2cu8,
+                    0x9fu8, 0x41u8, 0x9du8, 0x9fu8,
                 ],
-                output_str: ~"043f8582f241db351ce627e153e7f0e4",
+                output_str: ~"d174ab98d277d9f5a5611c2c9f419d9f",
             },
             Test {
-                input:
-                     ~"123456789012345678901234567890" +
-                      "123456789012345678901234567890" +
-                      "12345678901234567890",
+                input: ~"123456789012345678901234567890" +
+                        "123456789012345678901234567890" +
+                        "12345678901234567890",
                 output: ~[
-                    0x84u8, 0x98u8, 0x3Eu8, 0x44u8,
-                    0x1Cu8, 0x3Bu8, 0xD2u8, 0x6Eu8,
-                    0xBAu8, 0xAEu8, 0x4Au8, 0xA1u8,
-                    0xF9u8, 0x51u8, 0x29u8, 0xE5u8,
+                    0x57u8, 0xedu8, 0xf4u8, 0xa2u8,
+                    0x2bu8, 0xe3u8, 0xc9u8, 0x55u8,
+                    0xacu8, 0x49u8, 0xdau8, 0x2eu8,
+                    0x21u8, 0x07u8, 0xb6u8, 0x7au8,
                 ],
-                output_str: ~"e33b4ddc9c38f2199c3e7b164fcc0536",
-            },
-            Test {
-                input: a_million_letter_a(),
-                output: ~[
-                    0x34u8, 0xAAu8, 0x97u8, 0x3Cu8,
-                    0xD4u8, 0xC4u8, 0xDAu8, 0xA4u8,
-                    0xF6u8, 0x1Eu8, 0xEBu8, 0x2Bu8,
-                    0xDBu8, 0xADu8, 0x27u8, 0x31u8,
-                ],
-                output_str: ~"34aa973cd4c4daa4f61eeb2bdbad2731",
+                output_str: ~"57edf4a22be3c955ac49da2e2107b67a",
             },
         ];
-        // Examples from wikipedia
-
-        let wikipedia_tests = ~[
-            Test {
-                input: ~"The quick brown fox jumps over the lazy dog",
-                output: ~[
-                    0x2fu8, 0xd4u8, 0xe1u8, 0xc6u8,
-                    0x7au8, 0x2du8, 0x28u8, 0xfcu8,
-                    0xedu8, 0x84u8, 0x9eu8, 0xe1u8,
-                    0xbbu8, 0x76u8, 0xe7u8, 0x39u8,
-                    0x1bu8, 0x93u8, 0xebu8, 0x12u8,
-                ],
-                output_str: ~"2fd4e1c67a2d28fced849ee1bb76e7391b93eb12",
-            },
-            Test {
-                input: ~"The quick brown fox jumps over the lazy cog",
-                output: ~[
-                    0xdeu8, 0x9fu8, 0x2cu8, 0x7fu8,
-                    0xd2u8, 0x5eu8, 0x1bu8, 0x3au8,
-                    0xfau8, 0xd3u8, 0xe8u8, 0x5au8,
-                    0x0bu8, 0xd1u8, 0x7du8, 0x9bu8,
-                    0x10u8, 0x0du8, 0xb4u8, 0xb3u8,
-                ],
-                output_str: ~"de9f2c7fd25e1b3afad3e85a0bd17d9b100db4b3",
-            },
-        ];
-        let tests = fips_180_1_tests + wikipedia_tests;
-
-        // Test that it works when accepting the message all at once
-
-        let mut out = [0u8, ..16];
+        let tests = wikipedia_tests + test_vectors;
 
         let mut md = ~Md5::new();
+        let mut out = [0u8, ..16];
+
+        // Test that it works when accepting the message all at once
         for tests.iter().advance |t| {
             (*md).input_str(t.input);
             md.result(out);
@@ -393,7 +369,6 @@ fn test_md5() {
 
             md.reset();
         }
-
 
         // Test that it works when accepting the message in pieces
         for tests.iter().advance |t| {
@@ -408,7 +383,7 @@ fn test_md5() {
             assert!(t.output.as_slice() == out);
 
             let out_str = (*md).result_str();
-            assert_eq!(out_str.len(), 40);
+            assert_eq!(out_str.len(), 32);
             assert!(out_str == t.output_str);
 
             md.reset();
